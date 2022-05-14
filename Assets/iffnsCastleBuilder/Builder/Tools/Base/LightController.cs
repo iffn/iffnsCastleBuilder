8aed3262b878f5ef6b73f031cd05223420de7670
iffn <iffn1990@gmail.com>
2022-05-14 10:34:52 +0200

    Improved night light system

--- Assets/iffnsCastleBuilder/Builder/Tools/Base/LightController.cs
+++ Assets/iffnsCastleBuilder/Builder/Tools/Base/LightController.cs
@@ -8,6 +8,7 @@ namespace iffnsStuff.iffnsCastleBuilder
     {
         [SerializeField] Transform SunPlane;
         [SerializeField] Transform SunRotation;
+        [SerializeField] Light LinkedLight;
 
         float lightRotationAngleRad = Mathf.PI * 0.5f;
         public float LightRotationAngleRad
@@ -16,6 +17,27 @@ namespace iffnsStuff.iffnsCastleBuilder
             {
                 SunRotation.transform.localRotation = Quaternion.Euler(value * Mathf.Rad2Deg * Vector3.up);
                 lightRotationAngleRad = value;
+
+                float angle = value * Mathf.Rad2Deg;    
+                // 0 = morning
+                // 90 = noon
+                // -90 = midnight
+                // 180 = -180 = evening
+
+                if(angle < -20 && angle > -160)
+                {
+                    RenderSettings.ambientIntensity = 0.2f;
+                    RenderSettings.reflectionIntensity = 0.2f;
+                    LinkedLight.intensity = 0;
+                }
+                else
+                {
+                    RenderSettings.ambientIntensity = 1f;
+                    RenderSettings.reflectionIntensity = 1f;
+                    LinkedLight.intensity = 1;
+                }
+
+                //
             }
 
             get
