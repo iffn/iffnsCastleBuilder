4ddea2ff014e76fa4e28786c07442577baabc6b3
iffn <iffn1990@gmail.com>
2022-05-28 18:16:12 +0200

    Implemented first LayoutOrganizer

---
+++ Assets/iffnsCastleBuilder/Builder/UI/Prefabs/LayoutOrganizer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayoutOrganizer : MonoBehaviour
+{
+    [SerializeField] Transform Title; //Can be null
+    [SerializeField] List<LayoutOrganizer> ChildLayoutOrganizers;
+
+    RectTransform rectTransform;
+
+
+    public void Setup()
+    {
+        rectTransform = transform.GetComponent<RectTransform>();
+    }
+
+    public float SetAndReturnHeight()
+    {
+        if (ChildLayoutOrganizers.Count == 0) return rectTransform.sizeDelta.y;
+
+        float shouldBeHeight = 0;
+
+        foreach (LayoutOrganizer child in ChildLayoutOrganizers)
+        {
+            shouldBeHeight += child.SetAndReturnHeight();
+        }
+
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, shouldBeHeight);
+
+        return shouldBeHeight;
+    }
+
+    // Start is called before the first frame update
+    public void UpdateLayout()
+    {
+
+    }
+}
